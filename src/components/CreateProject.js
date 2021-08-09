81c25141b9a9488751635f3ad9e5f809dab53b67
Clifford <kingsleyclifford2016@gmail.com>
2021-08-09 17:58:52 +0000

    added line of code

--- src/components/CreateProject.js
+++ src/components/CreateProject.js
@@ -1,22 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 export default function CreateProject() {
+  const histroy = useHistory();
+  const [project, setProject] = useState({
+    cost: 0,
+    title: "",
+    location: "",
+    projectManager: "",
+  });
+
+  function handleChange(event) {
+    setProject({ ...project, [event.target.name]: event.target.value });
+  }
+
+  function sendProject(event) {
+    event.preventDefault();
+    axios
+      .post("http://localhost:8090/projects/add/", {
+        ...project,
+        cost: parseFloat(project.cost),
+      })
+      .then((res) => {
+        console.log(res.data);
+        histroy.push("/");
+      })
+      .catch((err) => err.response);
+  }
   return (
-    <form class="form">
+    <form className="form">
       <h2 className="form-heading">PROJECT FORM</h2>
       <p className="label" type="Title:">
-        <input placeholder="Write the title"></input>
+        <input
+          placeholder="Write the title"
+          name="title"
+          value={project.title}
+          onChange={handleChange}
+        ></input>
       </p>
       <p className="label" type="Cost:">
-        <input type="number" placeholder="How much will it cost?"></input>
+        <input
+          type="number"
+          placeholder="How much will it cost?"
+          name="cost"
+          value={project.cost}
+          onChange={handleChange}
+        ></input>
       </p>
       <p className="label" type="Location:">
-        <input placeholder="What is the location"></input>
+        <input
+          placeholder="What is the location"
+          name="location"
+          value={project.location}
+          onChange={handleChange}
+        ></input>
       </p>
       <p className="label" type="Project Manager:">
-        <input placeholder="Who is managing the project?"></input>
+        <input
+          placeholder="Who is managing the project?"
+          name="projectManager"
+          value={project.projectManager}
+          onChange={handleChange}
+        ></input>
       </p>
-      <button className="forn-btn">Send Request</button>
+      <button className="forn-btn" onClick={sendProject}>
+        Send Request
+      </button>
     </form>
   );
 }
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,59 +1,80 @@
 import React, { useState, useEffect } from "react";
 import Detail from "./Detail";
+import axios from "axios";
 
 export default function Details() {
   const [state, setState] = useState([]);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
-    setState([
-      {
-        title: "Migrating to Django",
-        cost: 2192,
-        projectId: 23455,
-        location: "Madina",
-        manager: "Kwesi Mattew",
-      },
-      {
-        title: "Changing Cloud Server",
-        cost: 192,
-        projectId: 3401,
-        location: "Accra",
-        manager: "Anthony Frank",
-      },
-      {
-        title: "AWS bucket to Store Images",
-        cost: 1102,
-        projectId: 455,
-        location: "Madina",
-        manager: "Kwesi Mattew",
-      },
-      {
-        title: "Change Image Rendering Algorithm",
-        cost: 5400,
-        projectId: 39051,
-        location: "Tema",
-        manager: "John Aboaji",
-      },
-      {
-        title: "Improving UI/UX Design",
-        cost: 3100,
-        projectId: 247736,
-        ocation: "Airpod City",
-        manager: "Peggy Jean",
-      },
-      {
-        title: "Backend Design",
-        cost: 4670,
-        projectId: 657736,
-        ocation: "Tema",
-        manager: "Sylvia Dan",
-      },
-    ]);
+    const fetchUsers = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get("http://localhost:8090/projects/");
+        setState(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    };
+    fetchUsers();
   }, []);
+  // useEffect(() => {
+  //   setState([
+  //     {
+  //       title: "Migrating to Django",
+  //       cost: 2192,
+  //       projectId: 23455,
+  //       location: "Madina",
+  //       manager: "Kwesi Mattew",
+  //     },
+  //     {
+  //       title: "Changing Cloud Server",
+  //       cost: 192,
+  //       projectId: 3401,
+  //       location: "Accra",
+  //       manager: "Anthony Frank",
+  //     },
+  //     {
+  //       title: "AWS bucket to Store Images",
+  //       cost: 1102,
+  //       projectId: 455,
+  //       location: "Madina",
+  //       manager: "Kwesi Mattew",
+  //     },
+  //     {
+  //       title: "Change Image Rendering Algorithm",
+  //       cost: 5400,
+  //       projectId: 39051,
+  //       location: "Tema",
+  //       manager: "John Aboaji",
+  //     },
+  //     {
+  //       title: "Improving UI/UX Design",
+  //       cost: 3100,
+  //       projectId: 247736,
+  //       ocation: "Airpod City",
+  //       manager: "Peggy Jean",
+  //     },
+  //     {
+  //       title: "Backend Design",
+  //       cost: 4670,
+  //       projectId: 657736,
+  //       ocation: "Tema",
+  //       manager: "Sylvia Dan",
+  //     },
+  //   ]);
+  // }, []);
   return (
-    <div className="details-box">
-      {state.map((detail, index) => (
-        <Detail detail={detail} key={index}></Detail>
-      ))}
-    </div>
+    <>
+      {!loading ? (
+        <div className="details-box">
+          {state.map((detail, index) => (
+            <Detail detail={detail} key={index}></Detail>
+          ))}
+        </div>
+      ) : (
+        <div className="text text-bold">Loding...</div>
+      )}
+    </>
   );
 }
